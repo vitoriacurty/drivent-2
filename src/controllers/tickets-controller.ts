d3b59e3b5f3e992e180f241ac437c49289c990a1
vitoriacurty <vitoria.curty@hotmail.com>
2023-09-25 13:02:19 -0300

    add tickets controller

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,7 +1,41 @@
-import { Request, Response } from "express";
+import { Response } from 'express';
+import httpStatus from 'http-status';
+import { AuthenticatedRequest } from '@/middlewares';
+import { ticketsServices } from '@/services/tickets-service';
 
-async function getTicket(req: Request, res: Response) {
+async function getTicketType(req: AuthenticatedRequest, res: Response) {
 
+    try {
+        const result = await ticketsServices.getTicketType();
+        res.status(httpStatus.OK).send(result);
+    } catch (error) {
+        return res.sendStatus(httpStatus.NO_CONTENT)
+    }
 }
 
-export const ticketsController = { getTicket }
+async function getTicket(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+
+    try {
+        const result = await ticketsServices.getTicket(userId);
+        return res.status(httpStatus.OK).send(result);
+    } catch (error) {
+        return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+}
+
+async function createTicket(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const { ticketTypeId } = req.body;
+
+    if (!ticketTypeId) return res.sendStatus(httpStatus.BAD_REQUEST);
+
+    try {
+        const result = await ticketsServices.createTicket(userId, ticketTypeId);
+        return res.status(httpStatus.CREATED).send(result);
+    } catch (error) {
+        return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+}
+
+export const ticketsController = { getTicket, getTicketType, createTicket };
