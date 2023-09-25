d27cc1ee238c3346071e2ad35708ae60e671e45a
vitoriacurty <vitoria.curty@hotmail.com>
2023-09-25 13:02:57 -0300

    add tickets service

--- src/services/tickets-service.ts
+++ src/services/tickets-service.ts
@@ -1,5 +1,37 @@
-async function getTicket() {
+import { TicketStatus } from '@prisma/client';
+import { notFoundError } from '@/errors';
+import { enrollmentRepository } from '@/repositories';
+import { ticketsRepository } from '@/repositories/tickets-repository';
 
+async function getTicketType() {
+    const ticketTypes = await ticketsRepository.getTicketType();
+    if (!ticketTypes) throw notFoundError();
+
+    return ticketTypes;
 }
 
-export const ticketsServices = { getTicket }
+async function getTicket(userId: number) {
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    if (!enrollment) throw notFoundError();
+
+    const result = await ticketsRepository.getTicket(enrollment.id);
+    if (!result) throw notFoundError();
+
+    return result;
+}
+
+async function createTicket(userId: number, ticketTypeId: number) {
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    if (!enrollment) throw notFoundError();
+
+    await ticketsRepository.createTicket({
+        ticketTypeId: ticketTypeId,
+        enrollmentId: enrollment.id,
+        status: TicketStatus.RESERVED,
+    });
+
+    const result = await ticketsRepository.getTicket(enrollment.id);
+    return result;
+}
+
+export const ticketsServices = { getTicket, getTicketType, createTicket };
